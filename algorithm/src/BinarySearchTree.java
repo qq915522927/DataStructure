5b0a9220dc66c9f63ecc8b2dde0d344f7781eab1
Tommy Wu <zhiwenw@vmware.com>
2020-01-03 08:49:03 +0800

    BST tree remove key

--- algorithm/src/BinarySearchTree.java
+++ algorithm/src/BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree<K extends Comparable, V> {
     class Node{
         public K k;
@@ -49,6 +52,78 @@ public class BinarySearchTree<K extends Comparable, V> {
         }
         return root;
     }
+    public V get(K key) throws Exception {
+        Node res = get(key, root);
+        return res.v;
+    }
+    private Node get(K key, Node root) throws Exception {
+        if(root == null){
+            throw new Exception("Not found key: " + key);
+        }
+        if(root.k.equals(key)){
+            return root;
+        }
+        if(key.compareTo(root.k)>0){
+            return get(key, root.right);
+        }
+        // key < root.k
+        return get(key, root.left);
+    }
+    private Node findMax(Node root){
+        if(root==null){
+            return null;
+        }
+        if(root.right == null){
+            return root;
+        }
+        return findMax(root.right);
+    }
+    private Node removeMax(Node root){
+        if(root==null){
+            return null;
+        }
+        if(root.right == null && root.left == null){
+            return null;
+        }
+        if(root.right == null && root.left != null){
+            return root.left;
+        }
+        root.right = removeMax(root.right);
+        return root;
+    }
+    public V removeKey(K key) throws Exception {
+        V res = get(key);
+        root = removeNode(root, key);
+        return res;
+    }
+    private Node removeNode(Node root, K key) throws Exception {
+        if(root == null){
+            throw new Exception("Not contains key: " + key.toString());
+        }
+        if(root.k.equals(key)){
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left!=null && root.right==null){
+                return root.left;
+            }
+            if(root.left==null && root.right!=null){
+                return root.right;
+            }
+            Node candidate = findMax(root.left);
+            removeMax(root.left);
+            candidate.left = root.left;
+            candidate.right = root.right;
+            return candidate;
+        }
+        if(key.compareTo(root.k)<0){
+            root.left = removeNode(root.left, key);
+        }
+        if(key.compareTo(root.k)>0){
+            root.right = removeNode(root.right, key);
+        }
+        return root;
+    }
     public String preTraverse(){
         // 前序遍历
         StringBuilder builder = new StringBuilder();
@@ -83,6 +158,24 @@ public class BinarySearchTree<K extends Comparable, V> {
         // key equal root.k
         return true;
     }
+    public String levelTraverse(){
+        StringBuilder builder = new StringBuilder();
+
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        builder.append("[");
+        while (!queue.isEmpty()){
+            Node node = queue.remove();
+            if(node != null){
+                builder.append(node.toString());
+                builder.append(", ");
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
 
     public String midTraverse(){
         // 前序遍历
@@ -106,7 +199,7 @@ public class BinarySearchTree<K extends Comparable, V> {
         return preTraverse();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int count = 20;
         Integer[] array = new Integer[count];
         for (int i = 0; i < count; i++) {
@@ -120,9 +213,19 @@ public class BinarySearchTree<K extends Comparable, V> {
         }
         System.out.println(bst);
         System.out.println(bst.midTraverse());
-        for (int i = 0; i < count + 1; i++) {
+        for (int i = 0; i < count; i++) {
             assert bst.contain(i);
             System.out.println(bst.contain(i));
+            bst.removeKey(i);
         }
+        System.out.println(bst);
+        BinarySearchTree<Integer, Integer> bst2 = new BinarySearchTree<>();
+        bst2.addEle(3, 5);
+        bst2.addEle(4, 5);
+        bst2.addEle(9, 5);
+        System.out.println(bst2.levelTraverse());
+        System.out.println(bst2.removeKey(3));
+        System.out.println(bst2.removeKey(4));
+        System.out.println(bst2.levelTraverse());
     }
 }
